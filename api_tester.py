e4a73608e203561abc2878666b6802ca946b6cd0
dina-fouad <dina_albarghouthi@yahoo.com>
2021-09-08 21:49:27 +0300

    class34

--- api_tester.py
+++ api_tester.py
@@ -1,10 +1,10 @@
 import fire
 import requests
 
-API_HOST = "http://localhost:8000"
-RESOURCE_URI = "things"
-USERNAME = "admin"
-PASSWORD = "admin"
+API_HOST = "https://cookies-api-93.herokuapp.com/"
+RESOURCE_URI = "cookies"
+USERNAME = "dina"
+PASSWORD = "dinadandan"
 
 
 class ApiTester:
